ded914ef5e35f0c7fc01bc3bcc031af5b41a729b
ColinMcD05 <colinmcd05@gmail.com>
2026-02-26 19:54:42 -0500

    Update TornadoMovement.cs

--- Assets/Colin/Scripts/Enemies/Tornado/TornadoMovement.cs
+++ Assets/Colin/Scripts/Enemies/Tornado/TornadoMovement.cs
@@ -1,16 +1,39 @@
+using Unity.Transforms;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class TornadoMovement : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    // Mutable Variable
+    [Header("Mutable Variable")]
+    public float moveSpeed;
+    [SerializeField] Transform[] waypoints;
 
-    // Update is called once per frame
+    // Mutable Variables within scripts only
+    int currentWaypoint;
+
+    // Immutable Variables
+    float reachDistance = 0.1f;
+
     void Update()
     {
-        
+        Movement();
+    }
+
+    void Movement()
+    {
+        if (waypoints == null || waypoints.Length == 0)
+        {
+            return;
+        }
+
+        Transform target = waypoints[currentWaypoint];
+
+        transform.position = Vector3.MoveTowards(transform.position, target.position, moveSpeed * Time.deltaTime);
+
+        if ((new Vector3(transform.position.x, 0, transform.position.z) - new Vector3(target.position.x, 0, target.position.z)).sqrMagnitude < reachDistance * reachDistance)
+        {
+            currentWaypoint = (currentWaypoint + 1) % waypoints.Length;
+        }
     }
 }
