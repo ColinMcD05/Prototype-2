1af64118de13a490f230e42d2849223236641f52
Colin McDaniel <colinmcd05@gmail.com>
2026-03-02 16:24:19 -0500

    Update CutScene.cs

--- Assets/Colin/Scripts/Transition/CutScene.cs
+++ Assets/Colin/Scripts/Transition/CutScene.cs
@@ -1,6 +1,15 @@
 using UnityEngine;
+using UnityEngine.Playables;
 
 public class CutScene : MonoBehaviour
 {
+    [SerializeField] PlayableDirector timeline;
 
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+
+        }
+    }
 }
