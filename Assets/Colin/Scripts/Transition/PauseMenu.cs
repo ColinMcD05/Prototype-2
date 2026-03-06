ab3ecb9a417898f894c19c72898242fb83f9f423
ColinMcD05 <colinmcd05@gmail.com>
2026-03-06 12:38:01 -0500

    Update PauseMenu.cs

--- Assets/Colin/Scripts/Transition/PauseMenu.cs
+++ Assets/Colin/Scripts/Transition/PauseMenu.cs
@@ -3,11 +3,34 @@ using UnityEngine.InputSystem;
 
 public class PauseMenu : MonoBehaviour
 {
-    [SerializeField] InputActionReference pause;
+    [SerializeField] InputActionReference pauseAction;
+    [SerializeField] GameObject pauseMenu;
 
-    private void OnEnable()
+    bool isPaused;
+
+    private void Awake()
     {
-        
+        pauseAction.action.performed += Pausing;
 
+    }
+
+    private void OnDestroy()
+    {
+        pauseAction.action.performed -= Pausing;
+    }
+
+    private void Pausing(InputAction.CallbackContext obj)
+    {
+        isPaused = !isPaused;
+        if (isPaused)
+        {
+            pauseMenu.SetActive(true);
+            Time.timeScale = 0;
+        }
+        else
+        {
+            pauseMenu.SetActive(false);
+            Time.timeScale = 1;
+        }
     }
 }
