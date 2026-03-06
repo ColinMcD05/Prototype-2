e765e15c0e24baa59557c88ac9fbfd7dd9c377b8
ColinMcD05 <colinmcd05@gmail.com>
2026-03-06 00:33:55 -0500

    Pause menu script start

---
+++ Assets/Colin/Scripts/Transition/PauseMenu.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] InputActionReference pause;
+
+    private void OnEnable()
+    {
+        
+
+    }
+}
