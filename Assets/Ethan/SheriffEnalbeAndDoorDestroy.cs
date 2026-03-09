c01181bf08959ea1b8caf6ec7c7a10e30c177b6e
Ethan <e_suter@outlook.com>
2026-03-09 01:16:59 -0400

    Sheriff Vision enable

---
+++ Assets/Ethan/SheriffEnalbeAndDoorDestroy.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class SheriffEnalbeAndDoorDestroy : MonoBehaviour
+{
+    public GameObject sheriffEyes;
+    public GameObject sheriffDoor;
+    public MonoBehaviour sheriffVision;
+    void OnTriggerEnter(Collider other)
+    {
+        MonoBehaviour[] scripts = sheriffEyes.GetComponents<MonoBehaviour>();
+        // Check if the object we hit has the "player" tag
+        if (other.CompareTag("Player"))
+        {
+
+            sheriffEyes.GetComponent<SheriffVision>().enabled = true;
+            Destroy(sheriffDoor);
+        }
+    }
+}
